0fd31ea34a92c66af36f917875afdf32f6661200
ezzedine-chemak <ezzedinechemak@gmail.com>
2023-09-11 19:19:21 +0100

    python fuctions

--- fundamentals/fundamentals/function.py
+++ fundamentals/fundamentals/function.py
@@ -1,3 +1,4 @@
+# 1 :
 x = [ [5,2,3], [15,8,9] ] 
 students = [
      {'first_name':  'Michael', 'last_name' : 'Bryant'},
@@ -9,9 +10,38 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 30} ]
 
+# 2: iterateDictionary
 def iterateDictionary(some_list):
     for dictionary in some_list:
         for key, value in dictionary.items():
-            print(f"{key}: {value}")
+            print(f"{key} - {value}")
 
 print (iterateDictionary(students))
+
+# 3 : iterateDictionary2(key_name, some_list)
+
+def iterateDictionary2(key_name, some_list) :
+    for dictionary in some_list:
+        if key_name in dictionary :
+            print(dictionary[key_name])
+        else :
+            print(f"{key_name}not found")
+
+
+print(iterateDictionary2('first_name',students))
+
+
+# 4:printInfo(some_dict)
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+print(dojo.items())
+
+def printInfo(some_dict) :
+    for key, value_list in some_dict.items():
+        print(f" {len(value_list)}  {key}")
+        for value in value_list:
+            print(f"{value}")
+
+print(printInfo(dojo))
